f2baa7a03f697e3f19462dc3fc45fe2c34dc7be9
Mike Gordon <vprmatrix55@gmail.com>
2021-12-27 15:51:34 -0700

    added jQuery outline

--- script.js
+++ script.js
@@ -5,19 +5,50 @@ var currentHour = moment().hour();
 $("#currentDay").text(currentDate);
 console.log(currentHour)
 
-var rowHour = parseInt($(this).attr('id').split('-')[1])
-//set local storage by declaring key and value variables
-// key -> .parent();
-// value -> .siblings(".description")
+$(".saveBtn").on("click", function () {
+
+    // key -> .parent(); parent is the time-block id
+    var time = $(this).parent().attr("id");
+    // value -> .siblings(".description")
+    var text = $(this).siblings(".description").val();
+
+        //set local storage by declaring key and value variables
+    localStorage.setItem(time, text);
+
+})
+
 
 //getItem
 
-$("row-9 .description").val(localStorage.getItem("row-9"));
+$("row9 .description").val(localStorage.getItem("row9"));
+$("row10 .description").val(localStorage.getItem("row10"));
+$("row11 .description").val(localStorage.getItem("row11"));
+$("row12 .description").val(localStorage.getItem("row12"));
+$("row13 .description").val(localStorage.getItem("row13"));
+$("row14 .description").val(localStorage.getItem("row14"));
+$("row15 .description").val(localStorage.getItem("row15"));
+$("row16 .description").val(localStorage.getItem("row16"));
+$("row17 .description").val(localStorage.getItem("row17"));
+
+$(".time-block").each(function () {
+    var rowHour = parseInt($(this).attr("id").split("hour")[1]);
+    if (rowHour < currentHour) {
+        $(this).addClass("past")
+    }
+    else if (rowHour == currentHour) {
+        $(this).addClass("present")
+    }
+    else {
+        $(this).addClass("future")
+    }
+
+})
 
 
 
 
-console.log(currentDay);
+
+console.log(currentDate);
 
 // need to get a block hour to compare to current hour
 // if current hour .present
