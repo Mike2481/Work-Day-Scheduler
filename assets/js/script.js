c7797f19aab2632e652f7924a54c5e9e4ecdfba9
Mike Gordon <vprmatrix55@gmail.com>
2021-12-27 17:06:28 -0700

    add time tracking and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,63 @@ var currentHour = moment().hour();
 $("#currentDay").text(currentDate);
 console.log(currentHour)
 
-var rowHour = parseInt($(this).attr('id').split('-')[1])
-//set local storage by declaring key and value variables
-// key -> .parent();
-// value -> .siblings(".description")
+
+$(".saveBtn").on("click", function () {
+
+    // key -> .parent(); parent is the time-block id
+    var time = $(this).parent().attr("id");
+    // value -> .siblings(".description")
+    var text = $(this).siblings(".description").val();
+
+        //set local storage by declaring key and value variables
+    localStorage.setItem(time, text);
+
+
+});
 
 //getItem
 
-$("row-9 .description").val(localStorage.getItem("row-9"));
+$("#row9 .description").val(localStorage.getItem("row9"));
+$("#row10 .description").val(localStorage.getItem("row10"));
+$("#row11 .description").val(localStorage.getItem("row11"));
+$("#row12 .description").val(localStorage.getItem("row12"));
+$("#row13 .description").val(localStorage.getItem("row13"));
+$("#row14 .description").val(localStorage.getItem("row14"));
+$("#row15 .description").val(localStorage.getItem("row15"));
+$("#row16 .description").val(localStorage.getItem("row16"));
+$("#row17 .description").val(localStorage.getItem("row17"));
+
+console.log(localStorage.getItem("row9"));
+    
+
+function checkTime () {
 
 
+$(".time-block").each(function () {
+    var rowHour = parseInt($(this).attr("id").split("row")[1]);
+    if (rowHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+    }
+    else if (rowHour == currentHour) {
+        $(this).addClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+    }
+    else {
+        $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+    }
+
+});
+    
+};
 
 
-console.log(currentDay);
+checkTime();
+console.log(currentDate);
 
 // need to get a block hour to compare to current hour
 // if current hour .present
